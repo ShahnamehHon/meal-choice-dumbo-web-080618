a844ecbb840db31490c3bd08486753736db2f927
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-17 18:11:45 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,7 @@
+require 'pry'
+
+def meal_choice(x="meat")
+end
+return meal_choice("vegan")
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
